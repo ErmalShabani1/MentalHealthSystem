ec31c9bfdd36bcc5a5a83db81eaede8ae1a3ff42
flor3nt <gitflorent@gmail.com>
2025-11-23 18:48:15 +0100

    Krijimi i pjeses se Ushtrimeve

---
+++ BackEnd/MentalHealthSystemManagement/MentalHealthSystemManagement.Infrastructure/Migrations/20251121214931_tabelaUshtrimet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MentalHealthSystemManagement.Infrastructure.Migrations
+{
+    public partial class tabelaUshtrimet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exercises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PsikologId = table.Column<int>(type: "int", nullable: false),
+                    PatientId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AssignedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercises", x => x.Id);
+
+                    table.ForeignKey(
+                        name: "FK_Exercises_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+
+                    table.ForeignKey(
+                        name: "FK_Exercises_Psikologet_PsikologId",
+                        column: x => x.PsikologId,
+                        principalTable: "Psikologet",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exercises_PatientId",
+                table: "Exercises",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exercises_PsikologId",
+                table: "Exercises",
+                column: "PsikologId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exercises");
+        }
+    }
+}
